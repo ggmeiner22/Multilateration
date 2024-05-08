6a5007b038862f54bcc5949ff4a64f3700b322b3
Garrett <ggmeiner22@gmail.com>
2024-05-08 17:26:58 -0400

    Deleted Test Files

--- test2.jl
+++
@@ -1,6 +0,0 @@
-# Read input from stdin (the text file provided via command prompt)
-for line in eachline(stdin)
-    # Process each line as needed
-    println("Received input: $line")
-    # Add your multiplication logic here
-end
